0cc9ec0ba519936b7da1f9657e36f88d930b4eb8
nikitasinha14 <nikitasinha833@gmail.com>
2026-03-17 13:54:47 +0000

    Implemented UC3 using String.join()

---
+++ UC3BannerApp.java
@@ -0,0 +1,52 @@
+public class UC3BannerApp {
+
+    public static void main(String[] args) {
+
+        // Line 1
+        String line1 = String.join("   ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                " ***** "
+        );
+
+        // Line 2
+        String line2 = String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "
+        );
+
+        // Line 3
+        String line3 = String.join("   ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                " ***** "
+        );
+
+        // Line 4
+        String line4 = String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *"
+        );
+
+        // Line 5
+        String line5 = String.join("   ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                " ***** "
+        );
+
+        // Print banner
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
+}
